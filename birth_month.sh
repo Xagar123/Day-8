6031b999b38d4279ad3fe995ce8a3fbcef8aee7e
Xagar123 <sagarcoc2000@gmail.com>
2022-06-05 18:03:14 +0530

    Birth month

--- birth_month.sh
+++ birth_month.sh
@@ -1,6 +1,21 @@
-declare -A birth_month
+no_of_individuals=50
+declare -A birth_day
+echo "The birthday month and year of $no_of_individuals individuals are: "
+for(( individual = 1; individual <= no_of_individuals; individual++ ))
+do
+    (( birth_month = RANDOM % 12 + 1 ))
+    (( birth_year = RANDOM % 2 + 92 ))
+    echo "individual $individual has birthday in month $birth_month year $birth_year "
 
-i=0
-birth_month=( 1 2 3 4 5 6 7 8 9 10 11 12 )
+    birth_day[$birth_month]+=" ${individual}"
+done
 
-echo ${birth_month[@]}
+for month in ${!birth_day[@]}
+do
+    echo "The individuals who are having birthday in month $month are: "
+    for individual in ${birth_day[$month]}
+    do
+        echo -n "$individual ";
+    done
+    echo
+done
